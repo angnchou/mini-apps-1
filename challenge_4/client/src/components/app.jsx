7d700b3ae2a39ecaadfe9c18c81cb767299c6b53
Angela Chou <angnchou@gmail.com>
2018-12-14 07:24:18 -0800

    connect to server

--- challenge_4/client/src/components/app.jsx
+++ challenge_4/client/src/components/app.jsx
@@ -7,13 +7,16 @@ const numCols = 7;
 const empty = 0;
 const player1 = 1;
 const player2 = -1;
-/*components: 
+/*state: 
 board - show all plays for both players
-gameEnded - false -> true when winner
-track plays for each player - playerOneMoves, playerTwoMoves
-hasWinner - false -> true -> message
+playerOneMoves - then alternate true/false for each player
+hasWinner - initially false -> true when winner is found -> message
 
-//prop - square - alternate between 3 colors
+//create board using array of arrays of 0, player one is 1, player 2 is -1 
+
+//find winner: check after each play, see if surrounding squares have the same value as the last played td
+    //if there is, check that td's surrounding squares
+    //if not, no winner yet
 
 */
 
@@ -30,8 +33,8 @@ class App extends React.Component {
       board.push(row);
     }
     this.state = {
-      playerOneMoves: true,
       board: board,
+      playerOneMoves: true,
       hasWinner: false //when true, alert "congratulations!"
     };
     this.renderBoard = this.renderBoard.bind(this);
@@ -39,6 +42,7 @@ class App extends React.Component {
     this.renderCols = this.renderCols.bind(this);
     this.putInCol = this.putInCol.bind(this);
   }
+  //replace 0 with 1 or -1 for each move
   //   [
   //     [0 0 0],
   //     [0 0 0]
@@ -53,7 +57,8 @@ class App extends React.Component {
   //     [0 0 1]   3
   // ]
 
-  //set state to show board displaying moves from each player
+  //copy board then set state to show board displaying moves from each player
+  //each play has a col number but should change the value on the board from the bottom up if it is currently 0
   putInCol(col) {
     const newBoard = [...this.state.board];
     for (let i = numRows - 1; i >= 0; i--) {
@@ -75,14 +80,7 @@ class App extends React.Component {
     });
   }
 
-  renderBoard() {
-    return (
-      <table>
-        <tbody>{this.renderRows()}</tbody>
-      </table>
-    );
-  }
-
+  //add className to each td for css for each player
   renderCols(cols) {
     return cols.map((col, index) => {
       let className = "";
@@ -106,7 +104,13 @@ class App extends React.Component {
     });
   }
 
-  //clicking on each td means game is happening
+  renderBoard() {
+    return (
+      <table>
+        <tbody>{this.renderRows()}</tbody>
+      </table>
+    );
+  }
 
   render() {
     return (
--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,3 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 let app = express();
+const port = 3003;
+
+app.use(express.static("./client/dist"));
+
+app.get("/connect4", (req, res) => {
+  res.send("Connect 4!");
+});
+
+app.listen(port, () => {
+  console.log(`mini app 4 listening on port ${port} !`);
+});
